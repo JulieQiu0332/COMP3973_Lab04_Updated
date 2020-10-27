239483c493ebe5ca189d5e6955044b68508a1fb4
JulieQiu0332 <61534881+JulieQiu0332@users.noreply.github.com>
2020-10-26 23:30:10 -0700

    Lab04

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Lab04.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Lab04.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Province> Provinces { get; set; }
+        public DbSet<City> Cities { get; set; }
+
+    }
+}
---
+++ Data/SampleData.cs
@@ -0,0 +1,94 @@
+﻿using Lab04.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab04.Data
+{
+    public class SampleData
+    {
+        public static void Initialize(IApplicationBuilder app)
+        {
+            using (var serviceScope = app.ApplicationServices.GetService<IServiceScopeFactory>().CreateScope())
+            {
+                var context = serviceScope.ServiceProvider.GetService<ApplicationDbContext>();
+                context.Database.EnsureCreated();
+
+                // Look for any teams.
+                if (context.Provinces.Any())
+                {
+                    return;   // DB has already been seeded
+                }
+
+                var provinces = GetProvinces().ToArray();
+                context.Provinces.AddRange(provinces);
+                context.SaveChanges();
+
+                var cities = GetCities(context).ToArray();
+                context.Cities.AddRange(cities);
+                context.SaveChanges();
+            }
+        }
+
+        public static List<Province> GetProvinces()
+        {
+            List<Province> provinces = new List<Province>() {
+            new Province() {
+                ProvinceCode ="BC",
+                ProvinceName ="British Columbia",
+                
+            },
+             new Province() {
+                ProvinceCode ="AB",
+                ProvinceName ="Alberta",
+
+            },
+             new Province() {
+                ProvinceCode ="MB",
+                ProvinceName ="Manitoba",
+
+            },
+     
+           
+            new Province() {
+                ProvinceCode ="NB",
+                ProvinceName ="New Brunswick",
+            },
+        };
+
+            return provinces;
+        }
+
+        public static List<City> GetCities(ApplicationDbContext context)
+        {
+            List<City> cities = new List<City>() {
+            new City {
+ 
+                CityName = "Victoria",
+                ProvinceCode = context.Provinces.Find("BC").ProvinceCode,
+                Population = 385999
+            },
+            
+            new City {
+
+                CityName = "Burnaby",
+                ProvinceCode = context.Provinces.Find("BC").ProvinceCode,
+                Population = 256276
+            },
+            new City {
+
+                CityName = "Yellowknifw",
+                ProvinceCode = context.Provinces.Find("AB").ProvinceCode,
+                Population = 20234
+            },
+            
+        };
+
+            return cities;
+        }
+
+    }
+}
